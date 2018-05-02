2f008305145e1ad1cd0206002d26984d8171b3b7
Martial Tardy <tardy.m02@gmail.com>
2018-05-02 05:04:04 +0200

    correction couleurs

--- src/Interface/InterfaceGraphique.java
+++ src/Interface/InterfaceGraphique.java
@@ -9,6 +9,7 @@ import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 /**
  * Created by tardy on 25/04/2018.
@@ -25,6 +26,8 @@ public class InterfaceGraphique extends JFrame{
     private double coutSolution;
     private Graphe graphe;
     private String title;
+    public List<Color> colorList;
+    public int indiceCurrentColor;
 
     // Constructor to set up the GUI components and event handlers
     public InterfaceGraphique(Solution solution, Graphe graphe, String title,double coutSolution) {
@@ -32,6 +35,9 @@ public class InterfaceGraphique extends JFrame{
         this.graphe = graphe;
         this.title = title;
         this.coutSolution = coutSolution;
+        colorList = new LinkedList<>();
+        indiceCurrentColor = 0;
+        setColors();
         JPanel jPanel = new JPanel();
 
         Font titleFont = new Font("Arial", Font.BOLD, 20);
@@ -50,6 +56,7 @@ public class InterfaceGraphique extends JFrame{
 
         JLabel label1 = new JLabel("Coût de cette solution: " + coutSolution);
         JPanel jPanel2 = new JPanel();
+
         label1.setBorder(titleCout);
         jPanel2.setBorder(titleChemin);
         ArrayList<JLabel> jLabelArrayList = new ArrayList<>();
@@ -64,12 +71,18 @@ public class InterfaceGraphique extends JFrame{
             chemin += " " + integer;
             lastInteger = integer;
         }
+        jLabelArrayList.add(new JLabel(chemin));
+        jPanel2.setLayout(new GridLayout( jLabelArrayList.size(),2));
         for (JLabel jLabel: jLabelArrayList) {
+            RectDraw rectDraw = new RectDraw(colorList.get(indiceCurrentColor));
+            jPanel2.add(rectDraw);
             jPanel2.add(jLabel);
+            System.out.println("ici" + colorList.get(indiceCurrentColor));
+            indiceCurrentColor++;
         }
-
+        indiceCurrentColor = 0;
         jPanel.setLayout(new GridLayout(2 ,1));
-        jPanel2.setLayout(new GridLayout(2 + jLabelArrayList.size(),1));
+
         jPanel.add(label1);
         jPanel.add(jPanel2);
 
@@ -95,9 +108,10 @@ public class InterfaceGraphique extends JFrame{
         @Override
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
+            System.out.println("draw: " + colorList);
             setBackground(Color.WHITE);
-            g.setColor(Color.BLUE);
-
+            g.setColor(colorList.get(indiceCurrentColor));
+            indiceCurrentColor++;
             ArrayList<Lieu> lieux = new ArrayList<>();
             for (Integer integer : solution.getListeSolution()) {
                 lieux.add(graphe.getLieux().get(integer));
@@ -111,7 +125,9 @@ public class InterfaceGraphique extends JFrame{
                     Lieu lieu2 = (Lieu) iterator.next();
                     if (lieu.getId() == 0 && lieu2.getId() == 0) {
                         Random rand = new Random();
-                        Color color = new Color(rand.nextInt(0xFFFFFF));
+                        Color color = colorList.get(indiceCurrentColor);
+                        System.out.println(color);
+                        indiceCurrentColor++;
                         g.setColor(color);
                     }
                     
@@ -120,12 +136,61 @@ public class InterfaceGraphique extends JFrame{
                             lieu2.getCoordonnees().getX()*7,
                             lieu2.getCoordonnees().getY()*7
                     );
+                    g.drawLine(lieu.getCoordonnees().getX()*7 +1,
+                            lieu.getCoordonnees().getY()*7 +1,
+                            lieu2.getCoordonnees().getX()*7 +1,
+                            lieu2.getCoordonnees().getY()*7+1
+                    );
                     lieu = lieu2;
                     g.drawString("" + lieu.getId(),lieu.getCoordonnees().getX()*7 + 5,lieu.getCoordonnees().getY()*7 + 5);
                 }
             }
+            indiceCurrentColor = 0;
 
         }
+    }
+
+    private class RectDraw extends JPanel {
+
+        Color color;
+        public RectDraw(Color color) {
+            super();
+            this.color = color;
+        }
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawRect(230,40,10,10);
+            g.setColor(color);
+            g.fillRect(230,40,10,10);
+        }
+    }
+
+    private void setColors(){
+        
+        ArrayList<Lieu> lieux = new ArrayList<>();
+        for (Integer integer : solution.getListeSolution()) {
+            lieux.add(graphe.getLieux().get(integer));
+        }
+        int i = 0;
+        Iterator iterator = lieux.iterator();
+        Lieu lieu = null;
+        lieu = (Lieu) iterator.next();
+        while (iterator.hasNext()) {
+            if (iterator.hasNext()) {
+                Lieu lieu2 = (Lieu) iterator.next();
+                if (lieu.getId() == 0 && lieu2.getId() == 0) {
+                    Random rand = new Random();
+                    int test = rand.nextInt(0xFFFFFF) * 2;
+                    Color color = new Color(test);
+                    colorList.add(color);
+                }
+
+                lieu = lieu2;
+            }
+        }
+        Random rand = new Random();
+        Color color = new Color(rand.nextInt(0xFFFFFF));
+        colorList.add(color);
     }
 
 }
